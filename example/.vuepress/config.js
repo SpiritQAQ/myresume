dcba201bde021f6ef8814dd5d474792a7c20f1e6
Zegel <spiritqaq@gmail.com>
2022-03-08 19:05:39 +0800

    chore: build test

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,15 +1,15 @@
 module.exports = {
   port: 5000,
-  base: '/',
-  title: "简历 - 毕云帆 - 前端开发",
+  base: './',
+  title: '简历 - 毕云帆 - 前端开发',
   dest: 'example/dist',
   theme: 'resume',
   themeConfig: {
     sidebar: [
       {
         collapsable: false,
-        children: ['/']
-      }
-    ]
-  }
+        children: ['/'],
+      },
+    ],
+  },
 }
